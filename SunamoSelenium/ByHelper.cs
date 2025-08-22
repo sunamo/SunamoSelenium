8d1fd0fb1bae6466f04c87a464e4963348cb01b2
Radek Jancik <radek.jancik@usys.cz>
2025-08-22 17:20:28 +0200

    25.8.22.1 ...

--- SunamoSelenium/ByHelper.cs
+++ SunamoSelenium/ByHelper.cs
@@ -1,7 +1,8 @@
-namespace SunamoSelenium;
+﻿namespace SunamoSelenium;
 
 public static class ByHelper
 {
+
     public static By ClassName(string classes)
     {
         return By.CssSelector("." + classes.Replace(" ", "."));
